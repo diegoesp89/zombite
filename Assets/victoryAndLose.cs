3a71676c089a2a422a6de1212fed7f94196d7aff
STRIFE-GIGA\Strife <diego.espinoza.vasquez@gmail.com>
2018-01-27 17:36:24 -0300

    Cursor and Spring! (again?)

--- Assets/victoryAndLose.cs
+++ Assets/victoryAndLose.cs
@@ -5,36 +5,50 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class victoryAndLose : MonoBehaviour {
-	public int humanCount;
-	public int humanDead;
+public class victoryAndLose : MonoBehaviour
+{
+    public int humanCount;
+    public int humanDead;
 
-	// Use this for initialization
-	void Start () {
-		FsmVariables.GlobalVariables.GetFsmInt("humans").Value = humanCount;
-		FsmVariables.GlobalVariables.GetFsmGameObject("derecha").Value = GameObject.FindGameObjectsWithTag("Right")[0];
-		FsmVariables.GlobalVariables.GetFsmGameObject("izquierda").Value = GameObject.FindGameObjectsWithTag("Left")[0];
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(FsmVariables.GlobalVariables.GetFsmInt("humans").Value == 0){
-			 Debug.Log("Ganaste");
 
-		}
-		
-	}
 
-	public void Zombified(){
-		FsmVariables.GlobalVariables.GetFsmInt("humans").Value--;
-		Debug.Log("Zombificado");
-	}
+    void OnGUI()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
+    }
+    void Start()
+    {
 
-	public void Dead(){
-		Debug.Log("Muerto");
-	}
+        FsmVariables.GlobalVariables.GetFsmInt("humans").Value = humanCount;
+        FsmVariables.GlobalVariables.GetFsmGameObject("derecha").Value = GameObject.FindGameObjectsWithTag("Right")[0];
+        FsmVariables.GlobalVariables.GetFsmGameObject("izquierda").Value = GameObject.FindGameObjectsWithTag("Left")[0];
+    }
 
-	public void Restart(){
-			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (FsmVariables.GlobalVariables.GetFsmInt("humans").Value == 0)
+        {
+            Debug.Log("Ganaste");
+
+        }
+
+    }
+
+    public void Zombified()
+    {
+        FsmVariables.GlobalVariables.GetFsmInt("humans").Value--;
+        Debug.Log("Zombificado");
+    }
+
+    public void Dead()
+    {
+        Debug.Log("Muerto");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
