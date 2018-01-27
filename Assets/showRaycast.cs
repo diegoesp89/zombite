8d92e795f8c761b94d7edbe8605ee51c45665e86
STRIFE-GIGA\Strife <diego.espinoza.vasquez@gmail.com>
2018-01-27 16:16:33 -0300

    Reset button, minor fixes

--- Assets/showRaycast.cs
+++ Assets/showRaycast.cs
@@ -4,15 +4,12 @@ using System.Collections;
 public class showRaycast : MonoBehaviour
 {
 	void Start(){
-		RaycastHit hit;
-
 	
 	}
 	
 	void Update () {
 		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		mouseWorldPosition.z = 0;
-		Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
 		Debug.DrawRay(transform.position, mouseWorldPosition  - transform.position, Color.green);
 
 	}
---
+++ Assets/victoryAndLose.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HutongGames.PlayMaker;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class victoryAndLose : MonoBehaviour {
+	public int humanCount;
+	public int humanDead;
+
+	// Use this for initialization
+	void Start () {
+		FsmVariables.GlobalVariables.GetFsmInt("humans").Value = humanCount;
+		FsmVariables.GlobalVariables.GetFsmGameObject("derecha").Value = GameObject.FindGameObjectsWithTag("Right")[0];
+		FsmVariables.GlobalVariables.GetFsmGameObject("izquierda").Value = GameObject.FindGameObjectsWithTag("Left")[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(FsmVariables.GlobalVariables.GetFsmInt("humans").Value == 0){
+			 Debug.Log("Ganaste");
+
+		}
+		
+	}
+
+	public void Zombified(){
+		FsmVariables.GlobalVariables.GetFsmInt("humans").Value--;
+		Debug.Log("Zombificado");
+	}
+
+	public void Dead(){
+		Debug.Log("Muerto");
+	}
+
+	public void Restart(){
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+	}
+}
